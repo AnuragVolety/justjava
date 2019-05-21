bea5f49757f44d090c5d3bb4e8be461707e07a81
colonP173 <vanurag.173@gmail.com>
2019-05-21 12:38:12 +0530

    Commit 3

--- justjavamodule/src/main/java/com/google/samples/apps/justjavamodule/MainActivity.java
+++ justjavamodule/src/main/java/com/google/samples/apps/justjavamodule/MainActivity.java
@@ -3,6 +3,7 @@ package com.google.samples.apps.justjavamodule;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void intent(View view) {
         Intent intent = new Intent("com.example.main.mainactivity");
         startActivity(intent);
     }
