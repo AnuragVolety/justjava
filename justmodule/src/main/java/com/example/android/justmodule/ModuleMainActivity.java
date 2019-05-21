f6bacb3be19ccbed7027e9ca545ebf14ad361107
colonP173 <vanurag.173@gmail.com>
2019-05-21 14:48:25 +0530

    Commit 9

--- justmodule/src/main/java/com/example/android/justmodule/ModuleMainActivity.java
+++ justmodule/src/main/java/com/example/android/justmodule/ModuleMainActivity.java
@@ -3,6 +3,7 @@ package com.example.android.justmodule;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class ModuleMainActivity extends AppCompatActivity {
 
@@ -10,13 +11,14 @@ public class ModuleMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_module_main);
-        Intent intent = null;
+        Intent intent;
         try {
-            intent = new Intent(this,Class.forName("com.example.android.justjava.myMainActivity"));
+            intent = new Intent(getApplicationContext()x,Class.forName("com.example.android.justjava.MainActivity"));
+            startActivity(intent);
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
+            Log.e( "onCreate: ","error in intent" );
         }
-        startActivity(intent);
 
     }
 }
