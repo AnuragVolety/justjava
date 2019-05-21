d0c496971ee750eaa28f9169e40655c7cd9f769f
colonP173 <vanurag.173@gmail.com>
2019-05-21 15:24:05 +0530

    Commit 13

--- justmodule/src/main/java/com/example/android/justmodule/ModuleMainActivity.java
+++ justmodule/src/main/java/com/example/android/justmodule/ModuleMainActivity.java
@@ -11,16 +11,7 @@ public class ModuleMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_module_main);
-        Intent intent;
-        try {
-            intent = new Intent(ModuleMainActivity.this, Class.forName("com.example.android.justjava.MainActivity"));
-            Log.e( "onCreate: ", ""+intent);
-            Log.e("onCreate: ","In module, on the way to main activity" );
-            startActivity(intent);
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-            Log.e( "onCreate: ","error in intent" );
-        }
-
+        Log.e("onCreate: ","In module, on the way to main activity" );
+        startActivity(new Intent("com.example.android.justjava.MainActivity"));
     }
 }
